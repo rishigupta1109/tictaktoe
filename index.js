5248b3913c1ecf13156184b3bfd7308c531e1688
Rishi <grishi634@gmail.com>
2023-04-21 20:00:19 +0530

    cpu more optimised

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var positions = [
   [0, 0, 0],
 ];
 var mode = 0; // 0 for player vs player and 1 for player vs cpu
+var fistTurn = true;
 const p1 = document.getElementsByClassName("p1")[0];
 const p2 = document.getElementsByClassName("p2")[0];
 p1.innerText = "Your Turn";
@@ -32,18 +33,18 @@ const startgame = (m) => {
   game.style.display = "flex";
   navbar.style.display = "flex";
   mode = m;
-  if (mode === 1) {
-    playercount++;
-    document.querySelector(".p22 .heading").innerText = "CPU";
-    positions[1][1] = "O";
-    setTimeout(() => {
-      playercount++;
-      document.getElementById("box11").innerText = "O";
-      document.getElementById(
-        "box11"
-      ).style.textShadow = `rgb(255 255 255) 0px 0px 4px, rgb(255 255 255) 0px 0px 11px, rgb(255 255 255) 0px 0px 19px, rgb(27 255 0) 0px 0px 40px, rgb(27 255 0) 0px 0px 80px, rgb(27 255 0) 0px 0px 90px, rgb(27 255 0) 0px 0px 100px, rgb(27 255 0) 0px 0px 150px`;
-    }, 500);
-  }
+  // if (mode === 1) {
+  //   playercount++;
+  //   document.querySelector(".p22 .heading").innerText = "CPU";
+  //   positions[1][1] = "O";
+  //   setTimeout(() => {
+  //     playercount++;
+  //     document.getElementById("box11").innerText = "O";
+  //     document.getElementById(
+  //       "box11"
+  //     ).style.textShadow = `rgb(255 255 255) 0px 0px 4px, rgb(255 255 255) 0px 0px 11px, rgb(255 255 255) 0px 0px 19px, rgb(27 255 0) 0px 0px 40px, rgb(27 255 0) 0px 0px 80px, rgb(27 255 0) 0px 0px 90px, rgb(27 255 0) 0px 0px 100px, rgb(27 255 0) 0px 0px 150px`;
+  //   }, 500);
+  // }
   Array.from(boxes).forEach((element) => {
     if (mode === 0) element.addEventListener("click", setvaluePVP);
     else element.addEventListener("click", setvaluePVC);
@@ -177,7 +178,7 @@ const reset = () => {
   Array.from(boxes).forEach((element) => {
     element.innerText = "";
   });
-  if (mode === 1) {
+  if (mode === 1 && !fistTurn) {
     playercount++;
     document.querySelector(".p22 .heading").innerText = "CPU";
     positions[1][1] = "O";
@@ -209,9 +210,11 @@ const playerWon = (char) => {
   if (char === "X") {
     // alert("player 1 Won");
     complete = true;
+    fistTurn = false;
     console.log("player 1 Won");
   } else {
     // alert("player 2 Won");
+    fistTurn = true;
     complete = true;
     console.log("player 2 Won");
   }
@@ -398,25 +401,21 @@ const setvaluePVC = (e) => {
           return;
         } else {
           setTimeout(() => {
-            let random = Math.floor(Math.random() * 9);
+            // let random = Math.floor(Math.random() * 9);
 
-            while (positions[Math.floor(random / 3)][random % 3]) {
-              random = Math.floor(Math.random() * 9);
-            }
+            // while (positions[Math.floor(random / 3)][random % 3]) {
+            //   random = Math.floor(Math.random() * 9);
+            // }
             var opPos = optimisedPosn();
+            // console.log(opPos);
             if (opPos[0] === -1) {
-              let box = document.getElementById(
-                `box${Math.floor(random / 3)}${random % 3}`
-              );
-              box.innerText = "O";
-              box.style.textShadow = `rgb(255 255 255) 0px 0px 4px, rgb(255 255 255) 0px 0px 11px, rgb(255 255 255) 0px 0px 19px, rgb(27 255 0) 0px 0px 40px, rgb(27 255 0) 0px 0px 80px, rgb(27 255 0) 0px 0px 90px, rgb(27 255 0) 0px 0px 100px, rgb(27 255 0) 0px 0px 150px`;
-              positions[Math.floor(random / 3)][random % 3] = "O";
-            } else {
-              let box = document.getElementById(`box${opPos[0]}${opPos[1]}`);
-              box.innerText = "O";
-              box.style.textShadow = `rgb(255 255 255) 0px 0px 4px, rgb(255 255 255) 0px 0px 11px, rgb(255 255 255) 0px 0px 19px, rgb(27 255 0) 0px 0px 40px, rgb(27 255 0) 0px 0px 80px, rgb(27 255 0) 0px 0px 90px, rgb(27 255 0) 0px 0px 100px, rgb(27 255 0) 0px 0px 150px`;
-              positions[opPos[0]][opPos[1]] = "O";
+              var opPos = optimalPosition("O");
             }
+            let box = document.getElementById(`box${opPos[0]}${opPos[1]}`);
+            box.innerText = "O";
+            box.style.textShadow = `rgb(255 255 255) 0px 0px 4px, rgb(255 255 255) 0px 0px 11px, rgb(255 255 255) 0px 0px 19px, rgb(27 255 0) 0px 0px 40px, rgb(27 255 0) 0px 0px 80px, rgb(27 255 0) 0px 0px 90px, rgb(27 255 0) 0px 0px 100px, rgb(27 255 0) 0px 0px 150px`;
+            positions[opPos[0]][opPos[1]] = "O";
+
             playercount++;
             p2.innerText = "Wait for your turn";
             p1.innerText = "Your Turn";
@@ -444,4 +443,146 @@ const setvaluePVC = (e) => {
       }
     }
   }
+};
+
+const checkWin = (char, check = false, tempPositions) => {
+  let Won = false;
+  for (let i = 0; i < 3; i++) {
+    let oneRow = true;
+    for (let j = 0; j < 3; j++) {
+      if (tempPositions[i][j] !== char) {
+        oneRow = false;
+      }
+    }
+    if (oneRow) {
+      winningrow = [0, i];
+      if (!check) setdash(winningrow);
+      Won = true;
+      return Won;
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    let oneRow = true;
+    for (let j = 0; j < 3; j++) {
+      if (tempPositions[j][i] !== char) {
+        oneRow = false;
+      }
+    }
+    if (oneRow) {
+      winningrow = [1, i];
+      if (!check) setdash(winningrow);
+      Won = true;
+      return Won;
+    }
+  }
+  let diagonal = true;
+  for (let i = 0; i < 3; i++) {
+    if (tempPositions[i][i] !== char) {
+      diagonal = false;
+    }
+  }
+  if (diagonal) {
+    winningrow = [2, 0];
+    if (!check) setdash(winningrow);
+    Won = true;
+    return Won;
+  }
+  // 02 11 20
+  let oppdiagonal = true;
+  for (let i = 0; i < 3; i++) {
+    if (tempPositions[i][2 - i] !== char) {
+      oppdiagonal = false;
+    }
+  }
+  if (oppdiagonal) {
+    winningrow = [2, 1];
+    if (!check) setdash(winningrow);
+    Won = true;
+    return Won;
+  }
+
+  return Won;
+};
+const completed = (tempPositions) => {
+  let bool = true;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (tempPositions[i][j] === 0) {
+        bool = false;
+      }
+    }
+  }
+  return bool;
+};
+
+const possibleWins = (i, j, player, tempPositions, turn) => {
+  tempPositions[i][j] = turn;
+  if (checkWin(player, true, tempPositions)) {
+    tempPositions[i][j] = 0;
+    return [1, 0, 0];
+  }
+  if (checkWin(player === "X" ? "O" : "X", true, tempPositions)) {
+    tempPositions[i][j] = 0;
+    return [0, 1, 0];
+  }
+  if (Completed(tempPositions)) return [0, 0, 1];
+  let ct = [0, 0, 0];
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (tempPositions[i][j] === 0) {
+        var a = possibleWins(
+          i,
+          j,
+          player,
+          tempPositions,
+          turn === "X" ? "O" : "X"
+        );
+        ct[0] += a[0];
+        ct[1] += a[1];
+        ct[2] += a[2];
+      }
+    }
+  }
+  tempPositions[i][j] = 0;
+  return ct;
+};
+
+const optimalPosition = (player) => {
+  let tempPositions = JSON.parse(JSON.stringify(positions));
+  console.log(tempPositions);
+  console.log(positions);
+  let max = -1,
+    pos1 = -1,
+    pos2 = -1;
+  let arr = [];
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (tempPositions[i][j] === 0) {
+        let ct = possibleWins(
+          i,
+          j,
+          player,
+          tempPositions,
+          player === "X" ? "X" : "O"
+        );
+        arr.push([ct, [i, j]]);
+        // console.log(ct);
+      }
+    }
+  }
+  arr.sort((a, b) => {
+    if (a[0][1] < b[0][1]) return -1;
+    else if (a[0][1] > b[0][1]) return 1;
+    else {
+      if (a[0][0] > b[0][0]) return -1;
+      else if (a[0][0] < b[0][0]) return 1;
+      else {
+        if (a[0][2] > b[0][2]) return -1;
+        else if (a[0][2] < b[0][2]) return 1;
+        else return 0;
+      }
+    }
+  });
+  console.log(arr);
+  return [arr[0][1][0], arr[0][1][1]];
 };
